c41a65c6c1d151717d93ba90f4ea9567b5a4461e
ryogaeng <fbgyrud828@naver.com>
2024-10-16 01:52:41 +0900

    [feat] BaseEntity설정(id, 생성일시, 수정일시, 삭제여부)

--- src/main/java/fukuoka/soongsil_carries_love/common/BaseEntity.java
+++ src/main/java/fukuoka/soongsil_carries_love/common/BaseEntity.java
@@ -1,4 +1,35 @@
 package fukuoka.soongsil_carries_love.common;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SoftDelete;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+// @SoftDelete(columnName = "is_deleted")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_deleted", columnDefinition = "TINYINT(1) DEFAULT 0", insertable = false, updatable = false)
+    private boolean isDeleted;
 }
