4740cd7ddc099ce26c899e0f4f56200f181c0285
ryogaeng <fbgyrud828@naver.com>
2024-11-18 22:24:12 +0900

    [fix] 엔티티 내용 재명명

--- src/main/java/fukuoka/soongsil_carries_love/domain/highschool/entity/Highschool.java
+++ src/main/java/fukuoka/soongsil_carries_love/domain/highschool/entity/Highschool.java
@@ -16,10 +16,13 @@ import java.util.List;
 public class Highschool extends BaseEntity {
 
     @Column(nullable = false, length = 255)
-    private String highSchoolName;  // 고등학교 이름
+    private String schoolName;  // 고등학교 이름
 
     @Column(nullable = false, unique = true, length = 20)
     private String schoolCode;      // 고유 학교 코드 (DynamoDB의 room_id로 사용 가능)
+
+    @Column(nullable = false, length = 4)
+    private String coeduType;       // 남녀공학구분(남/녀/남녀공학)
 
     // 1:N 관계 설정
     @OneToMany(mappedBy = "highschool")
