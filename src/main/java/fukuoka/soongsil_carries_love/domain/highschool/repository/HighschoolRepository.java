b07caa4c1f5cb2f03a2f2183e215ba9e6f178f31
ryogaeng <fbgyrud828@naver.com>
2024-11-18 22:24:38 +0900

    [feat] DB에 중복된 내용 조회를 위한 query문 작성

--- src/main/java/fukuoka/soongsil_carries_love/domain/highschool/repository/HighschoolRepository.java
+++ src/main/java/fukuoka/soongsil_carries_love/domain/highschool/repository/HighschoolRepository.java
@@ -2,12 +2,11 @@ package fukuoka.soongsil_carries_love.domain.highschool.repository;
 
 import fukuoka.soongsil_carries_love.domain.highschool.entity.Highschool;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface HighschoolRepository extends JpaRepository<Highschool, Long> {
-    Optional<Highschool> findBySchoolCode(String schoolCode);
-
-    void deleteBySchoolCode(String schoolCode);
-
+    @Query("SELECT h.schoolCode FROM Highschool h")
+    List<String> findAllSchoolCodes();
 }
