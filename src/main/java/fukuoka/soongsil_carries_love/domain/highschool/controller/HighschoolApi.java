466252f8b5dab626c5f2e4e1e5a21dedf5d8dd6d
ryogaeng <fbgyrud828@naver.com>
2024-11-18 22:23:19 +0900

    [fix] neis api 학교기본정보-고등학교 정보 fetch, save 관련 엔드포인트 수정

--- src/main/java/fukuoka/soongsil_carries_love/domain/highschool/controller/HighschoolApi.java
+++ src/main/java/fukuoka/soongsil_carries_love/domain/highschool/controller/HighschoolApi.java
@@ -5,22 +5,30 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 public interface HighschoolApi {
 
-    @Operation(summary = "[Highschool] 고등학교 정보 가져오기", description = "Neis Open API에서 고등학교 구분이 일반고, 특목고, 자율고, 특성화고인 학교만 가져옵니다.")
+    @Operation(summary = "[Highschool] 고등학교 정보 가져오기", description = "SCHUL_KND_SC_NM(학교종류명) = \"고등학교인 데이터\" 가져와 DB에 저장, school_code가 pk")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "OK, 성공"),
             @ApiResponse(responseCode = "401", description = "Unauthorized, 인증 실패"),
             @ApiResponse(responseCode = "500", description = "Internal Server Error, 서버 오류")
     })
-    @Parameters({
-            @Parameter(name = "KEY", description = "Neis API 인증키", required = true),
-            @Parameter(name = "Type", description = "응답 데이터 형식 (xml/json)", required = true, example = "json"),
-            @Parameter(name = "pIndex", description = "페이지 위치", required = false, example = "1"),
-            @Parameter(name = "pSize", description = "페이지 당 요청 숫자", required = false, example = "100")
-    })
+//    @Parameters({
+//            @Parameter(name = "KEY", description = "Neis API 인증키", required = true),
+//            @Parameter(name = "Type", description = "응답 데이터 형식 (xml/json)", required = true, example = "json"),
+//            @Parameter(name = "pIndex", description = "페이지 위치", required = false, example = "1"),
+//            @Parameter(name = "pSize", description = "페이지 당 요청 숫자", required = false, example = "100"),
+//            @Parameter(name = "SCHUL_KND_SC_NM", description = "학교 종류", required = false, example = "고등학교")
+//    })
     @PostMapping("/fetch")
-    String fetchAndSaveHighschool();
+    ResponseEntity<String> fetchAndSaveHighschoolData();
+
+
+    @Operation(summary = "[Highschool] 고등학교별 인원 수 순위 조회 API", description = "고등학교, 인원 수 순위 리스트")
+    @GetMapping("/ranking")
+    void getHighschoolRanking();
 }
--- src/main/java/fukuoka/soongsil_carries_love/domain/highschool/controller/HighschoolController.java
+++ src/main/java/fukuoka/soongsil_carries_love/domain/highschool/controller/HighschoolController.java
@@ -2,20 +2,33 @@ package fukuoka.soongsil_carries_love.domain.highschool.controller;
 
 import fukuoka.soongsil_carries_love.domain.highschool.service.HighschoolService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/Neis-api")
+@RequestMapping("")
 public class HighschoolController implements HighschoolApi{
 
     private final HighschoolService highschoolService;
 
-    @PostMapping("/fetch")
-    public String fetchAndSaveHighschool() {
-        highschoolService.fetchAndSaveHighschoolData();
-        return "Highschool data has been successfully fetched and saved.";
+    @PostMapping("/neis-api/fetch")
+    public ResponseEntity<String> fetchAndSaveHighschoolData() {
+        try {
+            highschoolService.fetchAndSaveHighschoolData();
+            return ResponseEntity.ok("Highschool data fetched and saved successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch and save Highschool data.");
+        }
+    }
+
+    @GetMapping("/highschool/ranking")
+    public void getHighschoolRanking() {
+        return ;
     }
 }
