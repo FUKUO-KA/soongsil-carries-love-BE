4b6b7ae862d9bfdd45599cb1df48778fb6a26385
ryogaeng <fbgyrud828@naver.com>
2024-11-18 22:23:53 +0900

    [fix] 고등학교 정보 fetch, save 관련 Dto 및 converter 수정

--- src/main/java/fukuoka/soongsil_carries_love/domain/highschool/converter/HighschoolConverter.java
+++ src/main/java/fukuoka/soongsil_carries_love/domain/highschool/converter/HighschoolConverter.java
@@ -1,32 +1,20 @@
 package fukuoka.soongsil_carries_love.domain.highschool.converter;
 
-import fukuoka.soongsil_carries_love.domain.highschool.dto.HighschoolResponseDto;
+import fukuoka.soongsil_carries_love.domain.highschool.dto.HighschoolFetchResponseDto;
 import fukuoka.soongsil_carries_love.domain.highschool.entity.Highschool;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class HighschoolConverter {
 
-    public Highschool convertToEntity(HighschoolResponseDto dto) {
+    /*
+     HighschoolResponseDto -> Highschool 엔티티
+     */
+    public Highschool toEntity(HighschoolFetchResponseDto highschoolFetchResponseDto) {
         return Highschool.builder()
-                .highSchoolName(dto.getHighSchoolName())
-                .schoolCode(dto.getSchoolCode())
+                .schoolCode(highschoolFetchResponseDto.getSchoolCode())
+                .schoolName(highschoolFetchResponseDto.getSchoolName())
+                .coeduType(highschoolFetchResponseDto.getCoeduType())
                 .build();
-    }
-
-    public HighschoolResponseDto convertToResponseDto(Highschool highschool) {
-        HighschoolResponseDto responseDto = new HighschoolResponseDto();
-        responseDto.setHighSchoolName(highschool.getHighSchoolName());
-        responseDto.setSchoolCode(highschool.getSchoolCode());
-        return responseDto;
-    }
-
-    public List<HighschoolResponseDto> convertToResponseDtoList(List<Highschool> highschools) {
-        return highschools.stream()
-                .map(this::convertToResponseDto)
-                .collect(Collectors.toList());
     }
 }
