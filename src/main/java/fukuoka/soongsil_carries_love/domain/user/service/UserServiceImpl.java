80fe9afa4bf8592d1120b4b9a0151df2ebb3fcac
ryogaeng <fbgyrud828@naver.com>
2024-11-22 03:46:41 +0900

    [fix] 아마 사용안할듯

--- src/main/java/fukuoka/soongsil_carries_love/domain/user/service/UserServiceImpl.java
+++ src/main/java/fukuoka/soongsil_carries_love/domain/user/service/UserServiceImpl.java
@@ -1,4 +1,24 @@
 package fukuoka.soongsil_carries_love.domain.user.service;
 
-public class UserServiceImpl {
+import fukuoka.soongsil_carries_love.domain.user.dto.JoinDto;
+import fukuoka.soongsil_carries_love.domain.user.entity.User;
+import fukuoka.soongsil_carries_love.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(JoinDto joinDto){
+        return userRepository.save(User.builder()
+                .email(joinDto.getEmail())
+                // 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(joinDto.getPassword()))
+                .build()).getId();
+    }
 }
