5bdec8a2f08a703c2861cd3f60cc306d826844af
ryogaeng <fbgyrud828@naver.com>
2024-11-22 03:44:12 +0900

    [fix] BaseEntity 무력화

--- src/main/java/fukuoka/soongsil_carries_love/common/BaseEntity.java
+++ src/main/java/fukuoka/soongsil_carries_love/common/BaseEntity.java
@@ -3,16 +3,13 @@ package fukuoka.soongsil_carries_love.common;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.SoftDelete;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-// @SoftDelete(columnName = "is_deleted")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @SuperBuilder
@@ -25,10 +22,6 @@ public abstract class BaseEntity {
     @CreatedDate
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
 
     @Column(name = "is_deleted", columnDefinition = "TINYINT(1) DEFAULT 0", insertable = false, updatable = false)
     private boolean isDeleted;
