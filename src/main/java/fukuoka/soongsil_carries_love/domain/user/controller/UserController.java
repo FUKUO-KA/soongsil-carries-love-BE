ad3dc61147ae23475970db629f87e226bb6ad084
ryogaeng <fbgyrud828@naver.com>
2024-11-22 17:08:34 +0900

    [refactor] user 테이블에 school_code 컬럼 생성을 통한 변환 로직 리팩토링

--- src/main/java/fukuoka/soongsil_carries_love/domain/user/controller/UserController.java
+++ src/main/java/fukuoka/soongsil_carries_love/domain/user/controller/UserController.java
@@ -21,33 +21,10 @@ public class UserController implements UserApi{
 
     @PostMapping("/join")   // 회원가입
     public ResponseEntity<String> joinUser(@ModelAttribute JoinDto joinDto) {
-        System.out.println(joinDto.getName() + "님 회원가입 되었습니다.");
+        System.out.println(joinDto.getNickname() + "님 회원가입 되었습니다.");
         joinService.joinProcess(joinDto);
         return ResponseEntity.ok("회원가입이 완료되었습니다.");
     }
-
-//    @PostMapping("/user")
-//    public String signup(AddUserRequestDto request){
-//        userService.save(request); // 회원 가입 메소드 호출
-//        return "redirect:/login"; // 회원 가입이 완료된 후 로그인 페이지로 이동
-//    }
-//
-//    @GetMapping("/login")
-//    public String login(){
-//        return "login completed";
-//    }
-//
-//    @GetMapping("/signup")
-//    public String signup(){
-//        return "signup completed";
-//    }
-//
-//    @GetMapping("/logout")
-//    public String logout(HttpServletRequest request, HttpServletResponse response){
-//        new SecurityContextLogoutHandler().logout(request, response,
-//                SecurityContextHolder.getContext().getAuthentication());
-//        return "redirect:/login";
-//    }
 
     @GetMapping("/{highschoolId}/gender")
     public void getGenderStats() {
--- src/main/java/fukuoka/soongsil_carries_love/domain/user/converter/UserConverter.java
+++ src/main/java/fukuoka/soongsil_carries_love/domain/user/converter/UserConverter.java
@@ -1,5 +1,6 @@
 package fukuoka.soongsil_carries_love.domain.user.converter;
 
+import fukuoka.soongsil_carries_love.domain.highschool.entity.Highschool;
 import fukuoka.soongsil_carries_love.domain.user.dto.JoinDto;
 import fukuoka.soongsil_carries_love.domain.user.entity.User;
 import org.springframework.stereotype.Component;
@@ -9,17 +10,18 @@ import java.time.LocalDateTime;
 @Component
 public class UserConverter {
 
-    public User toEntity(JoinDto joinDto, String encodedPassword) {
+    public User toEntity(JoinDto joinDto, String encodedPassword, Highschool highschool) {
         return User.builder()
-                .name(joinDto.getName())
+//                .name(joinDto.getName())      // 이름 안받음
                 .email(joinDto.getEmail())
-                .college(joinDto.getCollege())
-                .department(joinDto.getDepartment())
-                .nickname(joinDto.getNickname())
+                .college(joinDto.getCollege() != null ? joinDto.getCollege() : "") // 단과대학 (기본값: 빈 문자열)
+                .department(joinDto.getDepartment() != null ? joinDto.getDepartment() : "") // 학과/학부 (기본값: 빈 문자열)
+                .nickname(joinDto.getNickname())        // 닉네임
                 .password(encodedPassword) // 암호화된 패스워드 사용
                 .gender(joinDto.getGender())
-                .studentId(joinDto.getStudentId())
-                .highschool(joinDto.getHighschoolId()) // Highschool 객체 매핑
+                .studentId(joinDto.getStudentId()) // 학번
+                .highschool(highschool)
+                .schoolCode(highschool.getSchoolCode())
                 .isDeleted(false) // 초기값 false
                 .createdAt(joinDto.getCreatedAt() != null ? joinDto.getCreatedAt() : LocalDateTime.now()) // 현재 시각
                 .build();
