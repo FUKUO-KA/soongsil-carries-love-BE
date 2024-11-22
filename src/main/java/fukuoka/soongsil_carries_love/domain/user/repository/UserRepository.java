69e5e83d9c0e25637b80c5f1ab3df5393574da59
ryogaeng <fbgyrud828@naver.com>
2024-11-23 01:51:11 +0900

    [feat] findByEmail

--- src/main/java/fukuoka/soongsil_carries_love/domain/user/repository/UserRepository.java
+++ src/main/java/fukuoka/soongsil_carries_love/domain/user/repository/UserRepository.java
@@ -3,10 +3,8 @@ package fukuoka.soongsil_carries_love.domain.user.repository;
 import fukuoka.soongsil_carries_love.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email); // 이메일로 사용자 정보를 가져옴
-
     Boolean existsByEmail(String email);
+
+    User findByEmail(String email);
 }
