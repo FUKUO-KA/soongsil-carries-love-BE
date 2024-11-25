2ababe593c41a10940ac827bd7ccedbd70909a8d
Seungwoo, Joo <thisis.joos@gmail.com>
2024-11-25 21:47:51 +0900

    fix: 모든 출처에 대하여 CORS 허용

--- src/main/java/fukuoka/soongsil_carries_love/global/config/CorsMvcConfig.java
+++ src/main/java/fukuoka/soongsil_carries_love/global/config/CorsMvcConfig.java
@@ -13,7 +13,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
         corsRegistry.addMapping("/**")
                 .allowedOrigins("*") // 모든 출처 허용
                 .allowedMethods("*") // 모든 HTTP 메서드 허용
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true); // 쿠키 전송 허용 (필요 시 설정)
+                .allowedHeaders("*"); // 모든 헤더 허용
     }
 }
