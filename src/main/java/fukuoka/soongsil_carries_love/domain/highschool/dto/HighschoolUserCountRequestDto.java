76f9374aebc1b9692b0162b9ecc23d826616fd94
ryogaeng <fbgyrud828@naver.com>
2024-11-27 19:12:15 +0900

    [feat] 통계 api 구현 관련 Dto 생성

---
+++ src/main/java/fukuoka/soongsil_carries_love/domain/highschool/dto/HighschoolUserCountRequestDto.java
@@ -0,0 +1,10 @@
+package fukuoka.soongsil_carries_love.domain.highschool.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HighschoolUserCountRequestDto {
+    private String highschoolCode;
+}
