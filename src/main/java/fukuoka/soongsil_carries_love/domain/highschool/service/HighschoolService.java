ee892a7e3b49fec2d637197e4abf28445ae33530
ryogaeng <fbgyrud828@naver.com>
2024-11-27 19:13:25 +0900

    [feat] 통계 api 관련 로직 구현

--- src/main/java/fukuoka/soongsil_carries_love/domain/highschool/service/HighschoolService.java
+++ src/main/java/fukuoka/soongsil_carries_love/domain/highschool/service/HighschoolService.java
@@ -1,5 +1,9 @@
 package fukuoka.soongsil_carries_love.domain.highschool.service;
 
+import fukuoka.soongsil_carries_love.domain.highschool.dto.HighschoolGenderRatioResponseDto;
+import fukuoka.soongsil_carries_love.domain.highschool.dto.HighschoolRankingofUserCountResponseDto;
+import fukuoka.soongsil_carries_love.domain.highschool.dto.HighschoolUserCountResponseDto;
+import fukuoka.soongsil_carries_love.domain.highschool.dto.HighschoolUserStudentIdResponseDto;
 import fukuoka.soongsil_carries_love.domain.highschool.repository.HighschoolRepository;
 
 import java.util.List;
@@ -9,4 +13,13 @@ public interface HighschoolService {
     void fetchAndSaveHighschoolData();
 
     List<String> getAllSchoolNames();
+
+    HighschoolUserCountResponseDto getUserCountByHighschool(String highschoolCode);
+
+    HighschoolGenderRatioResponseDto getGenderRatioByHighschool(String highschoolCode);
+
+    HighschoolUserStudentIdResponseDto getStudentCountByYear(String highschoolCode);
+
+    List<HighschoolRankingofUserCountResponseDto> getUserCountByHighschool();
+
 }
