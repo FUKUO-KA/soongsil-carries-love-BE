b7972dddf4cbc11d174c7b2f9d754d4d262d1d12
Seungwoo, Joo <thisis.joos@gmail.com>
2024-11-29 14:09:24 +0900

    feat: allow CORS

---
+++ src/main/java/fukuoka/soongsil_carries_love/global/config/WebConfig.java
@@ -0,0 +1,16 @@
+package fukuoka.soongsil_carries_love.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(true); // 쿠키 인증 요청 허용
+    }
+}
